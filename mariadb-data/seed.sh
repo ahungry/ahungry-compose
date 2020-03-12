a5dc0143106f6e4808df50fc38c4165ec8a80630
Matthew Carter <m@ahungry.com>
2020-03-12 09:13:00 -0400

    Accepting connections now, nearly done

--- mariadb-data/seed.sh
+++ mariadb-data/seed.sh
@@ -2,7 +2,7 @@
 
 sleep 10
 
-echo 'create database ahungry;' | mysql -h mariadb -u root --password=ahungry ahungry &&
+echo 'create database ahungry;' | mysql -h mariadb -u root --password=ahungry &&
     gunzip -c /mnt/store/mariadb-data/*.sql.gz | mysql -h mariadb -u root --password=ahungry ahungry
 
 echo "Seeded!"
